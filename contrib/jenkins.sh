d2ac94ff0e0fe889519294fec1f2746a806e4b3f
Neels Hofmeyr <neels@hofmeyr.de>
2017-07-10 15:07:22 +0200

    split off osmo-mgw: remove files, apply build, rename
    Add vty and logging previously used from libcommon
    
    Rename libmgcp to libosmo-legacy-mgcp and install.
    
    Use DLMGCP, not DMGCP.
    
    Change-Id: I71a0a16ebaaef881c34235849601fc40aa12cfd7
    Depends: libosmocore I09c587e2d59472cbde852d467d457254746d9e67

--- contrib/jenkins.sh
+++ contrib/jenkins.sh
@@ -24,29 +24,17 @@ verify_value_string_arrays_are_terminated.py $(find . -name "*.[hc]")
 export PKG_CONFIG_PATH="$inst/lib/pkgconfig:$PKG_CONFIG_PATH"
 export LD_LIBRARY_PATH="$inst/lib"
 
-osmo-build-dep.sh libosmo-abis
-osmo-build-dep.sh libosmo-netif
-osmo-build-dep.sh libosmo-sccp
-PARALLEL_MAKE="" osmo-build-dep.sh libsmpp34
-osmo-build-dep.sh openggsn
-
-if [ "x$IU" = "x--enable-iu" ]; then
-	osmo-build-dep.sh libasn1c
-	#osmo-build-dep.sh asn1c aper-prefix # only needed for make regen in osmo-iuh
-	osmo-build-dep.sh osmo-iuh
-fi
-
 set +x
 echo
 echo
 echo
-echo " =============================== osmo-msc ==============================="
+echo " =============================== osmo-mgw ==============================="
 echo
 set -x
 
 cd "$base"
 autoreconf --install --force
-./configure --enable-osmo-bsc --enable-nat $SMPP $MGCP $IU --enable-vty-tests --enable-external-tests
+./configure $MGCP --enable-vty-tests --enable-external-tests
 $MAKE $PARALLEL_MAKE
 LD_LIBRARY_PATH="$inst/lib" $MAKE check \
   || cat-testlogs.sh
